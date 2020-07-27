3d97e2c1047372ad69082057133e1454b72b3f65
dmtevelavichius <tevelavichus@gmail.com>
2020-07-27 11:14:55 +0300

    Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        long priceTicket = 23000;
+        short variable = 20;
+        long totalMiles = priceTicket / variable;
+        System.out.println(totalMiles);
+    }
+}
+
